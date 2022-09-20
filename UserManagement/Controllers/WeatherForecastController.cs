fe402b659fdeed27e2326091a9f41bf258d20770
sajeshkumary <43872584+sajeshkumary@users.noreply.github.com>
2022-09-20 16:13:38 +0530

    Update WeatherForecastController.cs

--- UserManagement/Controllers/WeatherForecastController.cs
+++ UserManagement/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace UserManagement.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("WeatherForecastTest")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
